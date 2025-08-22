f95e925fb2d1c3fed9d1844a67787fadf3ab9e96
Huzaifa <islaminfokk@gmail.com>
2025-08-22 14:50:06 -0700

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,33 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express"
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
+
+ app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+
 const startServer = async () => {
   try {
-    const conn = await connectDB();
-    console.log("✅ MongoDB connected successfully:", conn.connection.host);
-
-    app.listen(ENV.PORT, () => {
-      console.log("🚀 Server running on", ENV.PORT);
-    });
-  } catch (err) {
-    console.log("❌ Failed to connect to MongoDB", err.message);
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("🚀 Server running on", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("❌ Error starting server", error);
+    process.exit(1);
   }
 };
 
